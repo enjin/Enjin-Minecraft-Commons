150c4a43624b942d0a76862fa683506987ebef4d
Evan Lindsay <evanlindsay@live.com>
2018-08-21 18:21:41 -0700

    Remove legacy menu api

--- spigot/src/main/java/com/enjin/minecraft_commons/spigot/ui/legacy/MenuClickBehavior.java
+++
@@ -1,8 +0,0 @@
-package com.enjin.minecraft_commons.spigot.ui.legacy;
-
-import org.bukkit.entity.Player;
-
-public abstract class MenuClickBehavior {
-    public void onClick(Player player) {
-    }
-}
--- spigot/src/main/java/com/enjin/minecraft_commons/spigot/ui/legacy/MenuCloseBehavior.java
+++
@@ -1,7 +0,0 @@
-package com.enjin.minecraft_commons.spigot.ui.legacy;
-
-import org.bukkit.entity.Player;
-
-public interface MenuCloseBehavior {
-    void onClose(Player player);
-}
